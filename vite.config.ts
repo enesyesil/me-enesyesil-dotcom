2e63837eedb7813b40b1dd69a35e97c3aed21457
Enes Yesil <me.enesyesil@gmail.com>
2026-01-22 02:12:53 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,17 @@ import { configDefaults } from 'vitest/config';
 export default defineConfig({
 	assetsInclude: ['**/*.md'],
 	plugins: [sveltekit()],
-	test: {
-	  globals: true,
-	  environment: 'jsdom',
-	  exclude: [...configDefaults.exclude, 'e2e/*'],
+	preview: {
+		port: 3000,
+		host: true,  // Listen on 0.0.0.0
 	},
-  });
+	server: {
+		port: 3000,
+		host: true,  // Listen on 0.0.0.0
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		exclude: [...configDefaults.exclude, 'e2e/*'],
+	},
+});
