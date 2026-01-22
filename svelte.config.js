fad165617142834cf856f3c51faa687ab2813ce2
Enes Yesil <me.enesyesil@gmail.com>
2026-01-22 15:28:48 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: [vitePreprocess({})],
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // Listen on all network interfaces
+      out: 'build',
+      precompress: false,
+      envPrefix: ''
+    })
   }
 };
 
