ecbb782eee09299c3da61692b6697b5ea5b7016e
Enes Yesil <me.enesyesil@gmail.com>
2026-02-17 17:57:06 -0500

    seo + metatags updated

---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let title: string = 'Enes Yesil | Software Engineer & Distributed Systems Enthusiast';
+	export let description: string =
+		"I'm a software developer interested in distributed systems, data infrastructure, and AI/ML. Currently in my final year of Computer Science at York University.";
+	export let keywords: string =
+		'Enes Yesil, Software Engineer, Distributed Systems, Data Infrastructure, AI, ML, York University';
+	export let image: string = 'https://enesyesil.me/favicon.svg';
+	export let url: string = 'https://enesyesil.me';
+	export let type: string = 'website';
+	export let site_name: string = 'Enes Yesil';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<meta name="keywords" content={keywords} />
+	<link rel="canonical" href={url} />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content={type} />
+	<meta property="og:url" content={url} />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={image} />
+	<meta property="og:site_name" content={site_name} />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={url} />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+	<meta property="twitter:image" content={image} />
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,10 @@
 	import '../app.pcss';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import SEO from '$lib/components/SEO.svelte';
 </script>
+
+<SEO />
 
 <div class="flex flex-col min-h-screen">
 	<Navbar />
